6a533591473f0fccf31510f8b6a76a127f28ac5c
PM-Cardoso <p.cardoso01@hotmail.com>
2024-05-13 12:40:46 +0100

    Added code for plotting the calibration curves for the uber model (different weight procedure).

--- 04.multivariate_analysis.R
+++ 04.multivariate_analysis.R
@@ -1,7 +1,7 @@
 ####################
 ## Description: 
 ##  - In this file we:
-##    - Fit a linear model to each of the variables for pooled therapies and by therapy.
+##    - Fit a linear model all the variables into one model.
 #################### 
 
 
@@ -14,6 +14,7 @@ source("code/00.set_up_data.R")
 # load libraries
 library(tidyverse)
 library(pROC)
+library(predtools)
 
 ###############################################################################
 ###############################################################################
@@ -39,7 +40,7 @@ ps.only_dataset <- readRDS("results/PS_model/ps.dataset_lm_all.rds")
 cprd_dataset <- cprd_dataset %>%
   left_join(
     ps.only_dataset %>%
-      select(patid, dstartdate, prop.score),
+      select(patid, dstartdate, prop.score.overlap, prop.score.IPW),
     by = c("patid", "dstartdate")
   )
 
@@ -52,20 +53,20 @@ cprd_dataset <- cprd_dataset %>%
 
 # stopdrug_3m_3mFU_MFN_hist
 # All therapies pooled
-lm_multi_all <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset, weights = prop.score, family=quasibinomial)
+lm_multi_all <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset, weights = prop.score.overlap, family=quasibinomial)
 
 # Each therapy
-lm_multi_DPP4 <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "DPP4"), weights = prop.score, family=quasibinomial)
-lm_multi_GLP1 <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "GLP1"), weights = prop.score, family=quasibinomial)
-lm_multi_MFN <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "MFN"), weights = prop.score, family=quasibinomial)
-lm_multi_SGLT2 <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "SGLT2"), weights = prop.score, family=quasibinomial)
-lm_multi_SU <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "SU"), weights = prop.score, family=quasibinomial)
-lm_multi_TZD <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "TZD"), weights = prop.score, family=quasibinomial)
+lm_multi_DPP4 <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "DPP4"), weights = prop.score.overlap, family=quasibinomial)
+lm_multi_GLP1 <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "GLP1"), weights = prop.score.overlap, family=quasibinomial)
+lm_multi_MFN <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "MFN"), weights = prop.score.overlap, family=quasibinomial)
+lm_multi_SGLT2 <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "SGLT2"), weights = prop.score.overlap, family=quasibinomial)
+lm_multi_SU <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "SU"), weights = prop.score.overlap, family=quasibinomial)
+lm_multi_TZD <- glm(stopdrug_3m_6mFU ~ dstartdate_dm_dur + dstartdate_age + prehba1c + preegfr + prebmi + prealt + pretotalcholesterol + gender + predrug_bloodmed + predrug_cardio_event + predrug_heart_event + predrug_micro_event + preckdstage + predrug_cld, data = cprd_dataset %>% filter(drugclass == "TZD"), weights = prop.score.overlap, family=quasibinomial)
 
 
 
 # ROC
-data.frame(
+plot_roc <- data.frame(
   Model = "Pooled",
   AUC = as.numeric(roc(cprd_dataset %>% select(stopdrug_3m_6mFU) %>% unlist(), predict(lm_multi_all, type = "response"), ci = TRUE)$ci)[2],
   LCI = as.numeric(roc(cprd_dataset %>% select(stopdrug_3m_6mFU) %>% unlist(), predict(lm_multi_all, type = "response"), ci = TRUE)$ci)[1],
@@ -127,11 +128,146 @@ data.frame(
 
 
 
+###############################################################################
+###############################################################################
+########################### UBER model analysis ###############################
+###############################################################################
+###############################################################################
+
+# roc(cprd_dataset %>% select(stopdrug_3m_6mFU) %>% unlist(), predict(lm_uber_model, type = "response"), ci = TRUE)
+
+formula_glm <- "stopdrug_3m_6mFU ~ 
+                            drugclass +
+                            dstartdate_dm_dur*drugclass + 
+                            dstartdate_age*drugclass + 
+                            prehba1c*drugclass + 
+                            preegfr*drugclass + 
+                            prebmi*drugclass + 
+                            prealt*drugclass + 
+                            pretotalcholesterol*drugclass + 
+                            gender*drugclass + 
+                            stopdrug_3m_3mFU_MFN_hist*drugclass +
+                            predrug_statins +
+                            predrug_bloodmed + 
+                            predrug_cardio_event + 
+                            predrug_heart_event + 
+                            predrug_micro_event + 
+                            preckdstage + 
+                            predrug_cld"
+
+
+# Overlap weights
+lm_uber_model.overlap_adjust <- glm(as.formula(paste(formula_glm, "+ drugline + numdrugs + smoking_cat + imd2015_10")), 
+                     data = cprd_dataset, weights = prop.score.overlap, family=quasibinomial)
+
+lm_uber_model.overlap <- glm(as.formula(formula_glm), 
+                                    data = cprd_dataset, weights = prop.score.overlap, family=quasibinomial)
+
+roc(cprd_dataset %>% select(stopdrug_3m_6mFU) %>% unlist(), predict(lm_uber_model.overlap, type = "response"), ci = TRUE)
+
+
+# IPW weights
+lm_uber_model.IPW_adjust <- glm(as.formula(paste(formula_glm, "+ drugline + numdrugs + smoking_cat + imd2015_10")), 
+                             data = cprd_dataset, weights = prop.score.IPW, family=quasibinomial)
+
+lm_uber_model.IPW <- glm(as.formula(formula_glm), 
+                                data = cprd_dataset, weights = prop.score.IPW, family=quasibinomial)
+
+roc(cprd_dataset %>% select(stopdrug_3m_6mFU) %>% unlist(), predict(lm_uber_model.IPW, type = "response"), ci = TRUE)
+
+# No weights
+lm_uber_model.no_weight_adjust <- glm(as.formula(paste(formula_glm, "+ drugline + numdrugs + smoking_cat + imd2015_10")), 
+                         data = cprd_dataset, family=quasibinomial)
+
+
+roc(cprd_dataset %>% select(stopdrug_3m_6mFU) %>% unlist(), predict(lm_uber_model.no_weight_adjust, type = "response"), ci = TRUE)
+
+
+probabilities.only_dataset <- data.frame(
+  patid = cprd_dataset$patid,
+  dstartdate = cprd_dataset$dstartdate,
+  
+  pred.overlap.MFN = predict(lm_uber_model.overlap, newdata = cprd_dataset %>% mutate(drugclass = factor("MFN", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.overlap.GLP1 = predict(lm_uber_model.overlap, newdata = cprd_dataset %>% mutate(drugclass = factor("GLP1", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.overlap.DPP4 = predict(lm_uber_model.overlap, newdata = cprd_dataset %>% mutate(drugclass = factor("DPP4", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.overlap.SGLT2 = predict(lm_uber_model.overlap, newdata = cprd_dataset %>% mutate(drugclass = factor("SGLT2", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.overlap.SU = predict(lm_uber_model.overlap, newdata = cprd_dataset %>% mutate(drugclass = factor("SU", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.overlap.TZD = predict(lm_uber_model.overlap, newdata = cprd_dataset %>% mutate(drugclass = factor("TZD", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  
+  pred.IPW.MFN = predict(lm_uber_model.IPW, newdata = cprd_dataset %>% mutate(drugclass = factor("MFN", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.IPW.GLP1 = predict(lm_uber_model.IPW, newdata = cprd_dataset %>% mutate(drugclass = factor("GLP1", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.IPW.DPP4 = predict(lm_uber_model.IPW, newdata = cprd_dataset %>% mutate(drugclass = factor("DPP4", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.IPW.SGLT2 = predict(lm_uber_model.IPW, newdata = cprd_dataset %>% mutate(drugclass = factor("SGLT2", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.IPW.SU = predict(lm_uber_model.IPW, newdata = cprd_dataset %>% mutate(drugclass = factor("SU", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.IPW.TZD = predict(lm_uber_model.IPW, newdata = cprd_dataset %>% mutate(drugclass = factor("TZD", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  
+  pred.no_weight.MFN = predict(lm_uber_model.no_weight_adjust, newdata = cprd_dataset %>% mutate(drugclass = factor("MFN", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.no_weight.GLP1 = predict(lm_uber_model.no_weight_adjust, newdata = cprd_dataset %>% mutate(drugclass = factor("GLP1", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.no_weight.DPP4 = predict(lm_uber_model.no_weight_adjust, newdata = cprd_dataset %>% mutate(drugclass = factor("DPP4", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.no_weight.SGLT2 = predict(lm_uber_model.no_weight_adjust, newdata = cprd_dataset %>% mutate(drugclass = factor("SGLT2", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.no_weight.SU = predict(lm_uber_model.no_weight_adjust, newdata = cprd_dataset %>% mutate(drugclass = factor("SU", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response"),
+  pred.no_weight.TZD = predict(lm_uber_model.no_weight_adjust, newdata = cprd_dataset %>% mutate(drugclass = factor("TZD", levels = levels(cprd_dataset$drugclass))) %>% as.data.frame(), type = "response")
+
+)
+
+saveRDS(probabilities.only_dataset, "results/Models/Predictions/model_predictions.rds")
 
 
 
 
+cprd_dataset <- cprd_dataset %>%
+  mutate(
+    pred.overlap = predict(lm_uber_model.overlap, type = "response"),
+    pred.overlap_adjust = predict(lm_uber_model.overlap_adjust, type = "response"),
+    pred.IPW = predict(lm_uber_model.IPW, type = "response"),
+    pred.IPW_adjust = predict(lm_uber_model.IPW_adjust, type = "response"),
+    pred.no_weight = predict(lm_uber_model.no_weight_adjust, type = "response")
+    )
 
 
+pdf("results/figures/uber_model_calibration.pdf", width = 4, height = 4)
+
+calibration_plot(
+  data = cprd_dataset %>% mutate(stopdrug_3m_6mFU = as.numeric(stopdrug_3m_6mFU)-1), 
+  obs = "stopdrug_3m_6mFU", 
+  pred = "pred.overlap", 
+  title = "Overlap weighting", 
+  y_lim = c(0, 0.3), 
+  x_lim=c(0, 0.3), 
+  nTiles = 10)
+calibration_plot(
+  data = cprd_dataset %>% mutate(stopdrug_3m_6mFU = as.numeric(stopdrug_3m_6mFU)-1), 
+  obs = "stopdrug_3m_6mFU", 
+  pred = "pred.overlap_adjust", 
+  title = "Overlap weighting + estimate adjustment", 
+  y_lim = c(0, 0.3), 
+  x_lim=c(0, 0.3), 
+  nTiles = 10)
+calibration_plot(
+  data = cprd_dataset %>% mutate(stopdrug_3m_6mFU = as.numeric(stopdrug_3m_6mFU)-1), 
+  obs = "stopdrug_3m_6mFU", 
+  pred = "pred.IPW", 
+  title = "IPW weighting", 
+  y_lim = c(0, 0.3), 
+  x_lim=c(0, 0.3), 
+  nTiles = 10)
+calibration_plot(
+  data = cprd_dataset %>% mutate(stopdrug_3m_6mFU = as.numeric(stopdrug_3m_6mFU)-1), 
+  obs = "stopdrug_3m_6mFU", 
+  pred = "pred.IPW_adjust", 
+  title = "IPW weighting + estimate adjustment", 
+  y_lim = c(0, 0.3), 
+  x_lim=c(0, 0.3), 
+  nTiles = 10)
+calibration_plot(
+  data = cprd_dataset %>% mutate(stopdrug_3m_6mFU = as.numeric(stopdrug_3m_6mFU)-1), 
+  obs = "stopdrug_3m_6mFU", 
+  pred = "pred.no_weight", 
+  title = "Model variables adjustment", 
+  y_lim = c(0, 0.3), 
+  x_lim=c(0, 0.3), 
+  nTiles = 10)
+
+dev.off()
 
 
