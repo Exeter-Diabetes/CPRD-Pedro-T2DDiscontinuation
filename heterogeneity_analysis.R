d56e703f9419cb75228d050bc2af52fb80ab6f1c
PM-Cardoso <p.cardoso01@hotmail.com>
2024-05-14 14:26:06 +0100

    In this file, we investigate the calibration of heterogeneous effects between 2 drug combinations of 6 total drugs.

---
+++ heterogeneity_analysis.R
@@ -0,0 +1,424 @@
+####################
+## Description: 
+##  - In this file we:
+##    - Investigate heterogeneity of therapy discontinuation
+#################### 
+
+
+# load working directory
+setwd("Samples/T2D_Discontinuation")
+
+# load functions
+source("code/00.set_up_data.R")
+
+# load libraries
+library(tidyverse)
+library(MatchIt)
+
+###############################################################################
+###############################################################################
+############################### Read Data In ##################################
+###############################################################################
+###############################################################################
+
+# load dataset
+cprd_dataset <- set_up_data(
+  raw_data = "20240308_t2d_1stinstance",
+  diagnosis = FALSE,
+  therapies = c("DPP4", "GLP1", "MFN", "SGLT2", "SU", "TZD"),
+  dataset = "full.dataset"
+) %>%
+  drop_na()
+
+
+# load propensity scores
+ps.only_dataset <- readRDS("results/PS_model/ps.dataset_lm_all.rds")
+
+
+# join propensity scores into dataset
+cprd_dataset <- cprd_dataset %>%
+  left_join(
+    ps.only_dataset %>%
+      select(patid, dstartdate, prop.score.overlap, prop.score.IPW),
+    by = c("patid", "dstartdate")
+  )
+
+
+probabilities.only_dataset <- readRDS("results/Models/Predictions/model_predictions.rds")
+
+
+# join predictions into dataset
+cprd_dataset <- cprd_dataset %>%
+  left_join(
+    probabilities.only_dataset,
+    by = c("patid", "dstartdate")
+  )
+
+
+
+
+###############################################################################
+###############################################################################
+####################### Per combination comparison ############################
+###############################################################################
+###############################################################################
+
+
+calc_ATE <- function(data, drugs, pred.variable = "no_weight", weight.variable = NULL, breakdown = NULL, matching = FALSE, ntiles = 10, caliper = 0.05, replace = FALSE, order = "random") {
+  
+  # output object
+  final_dataset <- NULL
+  
+  # calculation of all drug combinations (from provided)
+  combinations_matrix <- combn(drugs, 2)
+  
+  # number of combinations
+  n_comb <- ncol(combinations_matrix)
+  
+  # iterate through each combinations
+  for (comb_i in 1:n_comb) {
+    
+    # current drugs being analysed
+    current_drugs <- combinations_matrix[,comb_i]
+    
+    # collect only the drugs we are interested
+    data.initial <- data %>%
+      filter(drugclass %in% current_drugs) %>%
+      rename(
+        "pred.drug_1" = paste0("pred.", pred.variable, ".", current_drugs[1]),
+        "pred.drug_2" = paste0("pred.", pred.variable, ".", current_drugs[2]),
+      ) %>%
+      mutate(diff = pred.drug_1 - pred.drug_2) %>%
+      mutate(group = ntile(diff, ntiles))
+    
+    # if weight variable is provided, rename propensity score variable to default
+    if (!is.null(weight.variable)) {
+      data.initial <- data.initial %>%
+        rename(
+          "propensity.score" = paste0("prop.score.", weight.variable)
+        )
+    }
+    
+    # calculate the summary statistics for each ntile
+    predicted_treatment_effect <- data.initial %>%
+      plyr::ddply("group", dplyr::summarise,
+                  N = length(diff),
+                  diff.pred = mean(diff))
+    
+    # maximum number of deciles being tested
+    quantiles <- length(unique(data.initial[,"group"]))
+    
+    # create lists with results
+    mnumber = c(1:quantiles)
+    models  <- as.list(1:quantiles)
+    obs <- vector(); # lci <- vector(); uci <- vector()
+    
+    # iterate through deciles
+    for (i in mnumber) {
+      
+      # do this differently if "group" is categorical
+      if (is.factor(data.initial[,"group"])) {
+        # dataset being used in this quantile
+        data.new <- data.initial[data.initial[,"group"] == levels(data.initial[,"group"])[i],]
+      } else {
+        # dataset being used in this quantile
+        data.new <- data.initial[data.initial[,"group"] == i,]
+      }
+      
+      # If breakdown is not given (breakdown = variable to adjust by)
+      if (is.null(breakdown)) {
+        
+        # formula
+        formula <- "stopdrug_3m_6mFU ~ factor(drugclass)"
+        
+      } else {
+        
+        # variables used in adjustment
+        breakdown_adjust <- breakdown
+        # variables with only one variable represented
+        checker <- which(sapply(data.new[,breakdown_adjust], function(col) length(unique(col))) > 1)
+        
+        # formula
+        formula <- paste0("stopdrug_3m_6mFU ~ factor(drugclass) +", paste(breakdown_adjust[checker], collapse = "+"))
+        
+      }
+      
+      if (!is.null(weight.variable)) {
+        
+        if (matching == FALSE) {
+          
+          # fit linear regression for decile in the matched dataset
+          models[[i]] <- glm(as.formula("stopdrug_3m_6mFU ~ factor(drugclass)"), data=data.new, weights = propensity.score, family = quasibinomial())
+          
+        } else {
+          
+          # model if propensity scores are provided
+          matching_package_result <- MatchIt::matchit(
+            formula = formula("drugclass ~ prehba1c"), # shouldn't be used since we are specifying 'distance' (propensity scores)
+            data = data.new[which(data.new[,"group"] == i),], # select people in the quantile
+            method = "nearest",
+            distance = data.new[which(data.new[,"group"] == i),"propensity.score"],
+            replace = replace,
+            m.order = order,
+            caliper = caliper,
+            mahvars = NULL, estimand = "ATT", exact = NULL, antiexact = NULL, discard = "none", reestimate = FALSE, s.weights = NULL, std.caliper = TRUE, ratio = 1, verbose = FALSE, include.obj = FALSE,
+          )
+          
+          # fit linear regression for decile in the matched dataset
+          models[[i]] <- glm(as.formula("stopdrug_3m_6mFU ~ factor(drugclass)"), data=data.new, weights = matching_package_result$weights, family = binomial())
+          
+        }
+        
+      } else {
+        
+        # fit linear regression for decile in the matched dataset
+        models[[i]] <- glm(as.formula(formula),data=data.new, family = binomial())
+        
+      }
+      
+      preds.drug1 <- predict(models[[i]], newdata = data.new %>% filter(drugclass == current_drugs[1]), type = "response")
+      
+      preds.drug2 <- predict(models[[i]], newdata = data.new %>% filter(drugclass == current_drugs[2]), type = "response")
+      
+      obs <- append(obs, mean(preds.drug1) - mean(preds.drug2))
+      
+      # # calculate confidence intervals
+      # confint_all <- confint(models[[i]], levels=0.95)
+      # 
+      # # collect lower bound CI
+      # lci <- append(lci,confint_all[2,1])
+      # 
+      # # collect upper bound CI
+      # uci <- append(uci,confint_all[2,2])
+      
+    }
+    
+    # join treatment effects for deciles in a data.frame
+    effects <- data.frame(predicted_treatment_effect,cbind(obs))
+    
+    # returned list with fitted propensity model + decile treatment effects
+    final_dataset <- rbind(
+      final_dataset,
+      effects %>% mutate(Type = paste(current_drugs[1], "vs", current_drugs[2]))
+      )
+    
+  }
+  
+  return(final_dataset)
+  
+}
+
+# variables to adjust
+breakdown <- c(
+  # Extra info
+  "dstartdate_dm_dur", "dstartdate_age", "drugline", "numdrugs",
+  "smoking_cat", "imd2015_10", "gender",
+  # Biomarkers
+  "prehba1c", "preegfr", "prebmi", "prealt",
+  "pretotalcholesterol"
+)
+
+
+#:--------------
+## UBER model variable adjusted
+
+# ATE not adjusted
+
+ATE.var_adj.no_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight")
+
+# ATE adjusted for variables
+
+ATE.var_adj.var_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight", breakdown = breakdown)
+
+# ATE overlap weighted
+
+ATE.var_adj.overlap <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight", weight.variable = "overlap")
+
+# ATE overlap weighted
+
+ATE.var_adj.overlap_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight", weight.variable = "overlap", breakdown = breakdown)
+
+# ATE IPW weighted
+
+ATE.var_adj.IPW <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight", weight.variable = "IPW")
+
+# ATE IPW weighted
+
+ATE.var_adj.IPW_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight", weight.variable = "IPW", breakdown = breakdown)
+
+# ATE overlap matching
+
+ATE.var_adj.overlap_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight", weight.variable = "overlap", matching = TRUE)
+
+# ATE IPW matching
+
+ATE.var_adj.IPW_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "no_weight", weight.variable = "IPW", matching = TRUE)
+
+
+#:--------------
+## UBER model overlap weighted
+
+# ATE not adjusted
+
+ATE.overlap.no_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap")
+
+# ATE adjusted for variables
+
+ATE.overlap.var_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap", breakdown = breakdown)
+
+# ATE overlap weighted
+
+ATE.overlap.overlap <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap", weight.variable = "overlap")
+
+# ATE overlap weighted
+
+ATE.overlap.overlap_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap", weight.variable = "overlap", breakdown = breakdown)
+
+# ATE IPW weighted
+
+ATE.overlap.IPW <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap", weight.variable = "IPW")
+
+# ATE IPW weighted
+
+ATE.overlap.IPW_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap", weight.variable = "IPW", breakdown = breakdown)
+
+# ATE overlap matching
+
+ATE.overlap.overlap_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap", weight.variable = "overlap", matching = TRUE)
+
+# ATE IPW matching
+
+ATE.overlap.IPW_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap", weight.variable = "IPW", matching = TRUE)
+
+
+#:--------------
+## UBER model overlap weighted + var adjusted
+
+# ATE not adjusted
+
+ATE.overlap_adj.no_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust")
+
+# ATE adjusted for variables
+
+ATE.overlap_adj.var_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust", breakdown = breakdown)
+
+# ATE overlap weighted
+
+ATE.overlap_adj.overlap <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust", weight.variable = "overlap")
+
+# ATE overlap weighted
+
+ATE.overlap_adj.overlap_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust", weight.variable = "overlap", breakdown = breakdown)
+
+# ATE IPW weighted
+
+ATE.overlap_adj.IPW <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust", weight.variable = "IPW")
+
+# ATE IPW weighted
+
+ATE.overlap_adj.IPW_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust", weight.variable = "IPW", breakdown = breakdown)
+
+# ATE overlap matching
+
+ATE.overlap_adj.overlap_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust", weight.variable = "overlap", matching = TRUE)
+
+# ATE IPW matching
+
+ATE.overlap_adj.IPW_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "overlap_adjust", weight.variable = "IPW", matching = TRUE)
+
+
+#:--------------
+## UBER model IPW weighted
+
+# ATE not adjusted
+
+ATE.IPW.no_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW")
+
+# ATE adjusted for variables
+
+ATE.IPW.var_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW", breakdown = breakdown)
+
+# ATE overlap weighted
+
+ATE.IPW.overlap <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW", weight.variable = "overlap")
+
+# ATE overlap weighted
+
+ATE.IPW.overlap_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW", weight.variable = "overlap", breakdown = breakdown)
+
+# ATE IPW weighted
+
+ATE.IPW.IPW <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW", weight.variable = "IPW")
+
+# ATE IPW weighted
+
+ATE.IPW.IPW_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW", weight.variable = "IPW", breakdown = breakdown)
+
+# ATE overlap matching
+
+ATE.IPW.overlap_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW", weight.variable = "overlap", matching = TRUE)
+
+# ATE IPW matching
+
+ATE.IPW.IPW_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW", weight.variable = "IPW", matching = TRUE)
+
+
+#:--------------
+## UBER model IPW weighted + var adjusted
+
+# ATE not adjusted
+
+ATE.IPW_adj.no_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust")
+
+# ATE adjusted for variables
+
+ATE.IPW_adj.var_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust", breakdown = breakdown)
+
+# ATE overlap weighted
+
+ATE.IPW_adj.overlap <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust", weight.variable = "overlap")
+
+# ATE overlap weighted
+
+ATE.IPW_adj.overlap_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust", weight.variable = "overlap", breakdown = breakdown)
+
+# ATE IPW weighted
+
+ATE.IPW_adj.IPW <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust", weight.variable = "IPW")
+
+# ATE IPW weighted
+
+ATE.IPW_adj.IPW_adj <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust", weight.variable = "IPW", breakdown = breakdown)
+
+# ATE overlap matching
+
+ATE.IPW_adj.overlap_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust", weight.variable = "overlap", matching = TRUE)
+
+# ATE IPW matching
+
+ATE.IPW_adj.IPW_match <- calc_ATE(cprd_dataset, drugs = c("MFN", "DPP4", "GLP1", "SGLT2", "SU", "TZD"), pred.variable = "IPW_adjust", weight.variable = "IPW", matching = TRUE)
+
+
+
+
+
+
+
+
+
+
+# pdf("results/figures/heterogeneity_analysis.pdf", width = 10, height = 10)
+
+ATE.IPW_adj.IPW_match %>%
+  ggplot() +
+  geom_abline(aes(intercept = 0, slope = 1), colour = "red") +
+  geom_hline(aes(yintercept = 0), colour = "grey") +
+  geom_vline(aes(xintercept = 0), colour = "grey") +
+  geom_point(aes(y = obs, x = diff.pred)) + 
+  scale_x_continuous("Predicted Conditional Average Treatment Discontinuation", labels = scales::percent, breaks = seq(-1, 1, 0.02)) +
+  scale_y_continuous("Decile Average Treatment Discontinuation", labels = scales::percent, breaks = seq(-1, 1, 0.02)) +
+  facet_wrap(~Type, scales = "free") +
+  theme_bw()
+
+# dev.off()
+
