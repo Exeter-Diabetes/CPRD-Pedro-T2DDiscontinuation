8a7da28c86ef6b65800d291d2e5e526a2df9738c
PM-Cardoso <p.cardoso01@hotmail.com>
2024-05-16 10:51:41 +0100

    Make sure the propensity score model is fitted to all patients with 3m discontinuation data.

--- 02.propensity_score_lm.R
+++ 02.propensity_score_lm.R
@@ -29,7 +29,7 @@ cprd_dataset <- set_up_data(
   therapies = c("DPP4", "GLP1", "SGLT2", "SU", "TZD"),
   dataset = "3m.disc.dataset"
 ) %>%
-  drop_na()
+  drop_na(-stopdrug_6m_6mFU, -stopdrug_12m_6mFU)
 
 
 ########
